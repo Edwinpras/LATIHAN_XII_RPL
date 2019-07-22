94794a44b025999e8432e203eb909827cd2c754d
Chanif2003 <53119919+Chanif2003@users.noreply.github.com>
2019-07-22 23:15:45 +0700

    Update segitiga_bintang.php

--- segitiga_bintang.php
+++ segitiga_bintang.php
@@ -1,15 +1,46 @@
 <?php
-//mencetak bintang dengan perulangan
+//mencetak bintang dengan perulangan berjajar
 for($i=1;$i<=5;$i++){
 echo "*";
 }
-echo "<br/>";
 
+echo "<br/><br/><br/>";
+
+//mencetak bintang dengan perulangan menurun
 for($i=1;$i<=5;$i++){
 echo "*<br/>";
 }
 
+echo "<br/><br/><br/>";
+
+//mencetak segitiga bintang dengan perulangan
 for($i=1;$i<=5;$i++){
+  for ($a=1; $a<=$i;$a++){
+    echo "*";
+  }
+  echo "<br/>";
+}
+
+echo "<br/><br/><br/>";
+
+//mencetak segitiga bintang dengan perulangan
+for($i=1;$i<=5;$i++){
+  for ($z=5;$z>$i;$z--){
+	echo " ";
+  }
+  for ($a=1; $a<=$i;$a++){
+    echo "*";
+  }
+  echo "<br/>";
+}
+
+echo "<br/><br/><br/>";
+
+//mencetak segitiga bintang dengan perulangan
+for($i=1;$i<=5;$i++){
+  for ($z=5;$z>$i;$z--){
+	echo "-";
+  }
   for ($a=1; $a<=$i;$a++){
     echo "*";
   }
